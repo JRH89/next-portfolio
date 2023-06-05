f8f14d02989f0e286bcfe7bca760bad66992862c
JRH89 <thegemlins@gmail.com>
2023-06-05 09:34:26 -0700

    ok

--- app/support/page.js
+++ app/support/page.js
@@ -35,6 +35,7 @@ export default function Settings() {
 			if (response.ok) {
 				setShowMessage(true);
 				event.target.reset();
+				console.log(data)
 			} else {
 				setShowMessage(false);
 				console.log(data);
