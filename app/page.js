4e42cc1fdaddb2a04471ef19f42653401c1d6b8e
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-06-05 21:17:42 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -243,7 +243,7 @@ const Page = () => {
           </div>
         )}
         <div className="rounded-top-xl fixed bottom-0 w-full  flex justify-center">
-          <footer className="w-full font-bold max-w-[800px] text-center bg-gradient-to-r rounded-t-xl from-blue-500 via-green-500 to-red-500 text-slate-900">Hooker Hill Studios ⓒ 2023</footer>
+          <footer className="w-full z-50 font-bold max-w-[800px] text-center bg-gradient-to-r rounded-t-xl from-blue-500 via-green-500 to-red-500 text-slate-900">Hooker Hill Studios ⓒ 2023</footer>
         </div>
       </div></>
   );
