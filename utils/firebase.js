fab54649e6a4decfef5fe8fd86340478ce653301
JRH89 <thegemlins@gmail.com>
2023-06-07 22:16:39 -0700

    okkkkkk

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_APIKEY,
 	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
